d9ae99592e06b6675b15235e4bd73288896cde33
Santosh Kumar <kasantosh@yahoo.com>
2019-05-10 11:46:18 -0400

    Added seeds.sql and updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection(function() {
-  host: "localhost";
-  port: "PORT";
-  user: "root";
-  password: "mysqlpass123456";
-  database: burgers_db;
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "mysqlpass123456",
+  database: "burgers_db"
 });
+
+connection.connect(function() {
+  if (error) throw error;
+
+  console.log("Connected to database ...");
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT into burgers
+    (burger_name, devoured)
+VALUES
+    ("HamnCheese Burger", false),
+    ("Bacon-Cheese Burger", false),
+    ("Veggie Delite Burger", false);
